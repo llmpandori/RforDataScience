f66cf6f76347649a90350bd9c4116c94f5d8248c
llmpandori <lmcquinn@uci.edu>
2020-04-28 11:10:24 -0700

    Final edits before class on 4/29

--- Squirrel_Data_Showdown.R
+++ Squirrel_Data_Showdown.R
@@ -58,23 +58,68 @@ View(nyc_squirrels)
   # doesn't work w dates in this example, but let's change a col type
   
 ##### 4 Important Tidyr Functions with LL #####
+# Note: bio dataset belongs to LL and is not open access (until published)
+  
+# pivot
+  # pivot longer
+  long_bio <- bio %>%
+    pivot_longer(c(BB10A.CT:BB18A.O), names_to = "sample", values_to = "abundance")
+  View(long_bio)
+  
+  # gather is a similar function
+  long2 <- gather(bio, Sample, abundance, BB10A.CT:BB18A.O, factor_key=TRUE)
+  View(long2)
+  ?gather
+  
+  # pivot_wider
+  ?pivot_wider
+  fish_encounters
+  fish_encounters %>%
+    pivot_wider(names_from = station, values_from = seen)
+  # Fill in missing values
+  fish_encounters %>%
+    pivot_wider(names_from = station, values_from = seen, values_fill = list(seen = 0))
+  
+  # do the same thing with spread
+  ?spread
+  fish_encounters %>% 
+    spread(station, seen, fill = 0)
+  
+  
+# separate and unite using squirrel data
+  nyc_squirrels
+  nyc_squirrels_new <- nyc_squirrels %>%
+    separate(unique_squirrel_id, into = c("Hectare_ID", "Shift", "Date", "Hectare_Squirrel_Number"))
+  View(nyc_squirrels_new)
+  
+  #Unite them dack into squirrel ID#
+  nyc_squirrels_new <- nyc_squirrels_new %>%
+    unite("unique_squirrel_ID", Hectare_ID, Shift, Date, Hectare_Squirrel_Number)
+  
+  ####complete cases####
+  fun_squirrels <- nyc_squirrels_new %>%
+    complete(other_activities)
+  
+  fun_squirrels <- fun_squirrels %>%
+    fill(highlight_fur_color)
+  View(fun_squirrels)
  
 ##### Squirrel Data Showdown Challenge #####
   # Step 1 -  Load data (practice loading the nyc_squirrels 2 csv)
 
-  
+
   
   # Step 2 - Create a date2 column that contains formatted dates
   # How do you check your work?
-
+  
   
   
   # Step 3 - Separate the original date column into month, day and year? (try without lubridate) and then unite them.
-  
+
   
   
   # Step 4 - Change the location column into 2 separate colunns with the headings 'above ground' and 'ground plane' and make the values logical statements (true/false)
-  
+
   
   
   # Step 5 - Can you create a plot of your choice and post it as a reply to the thread @PandoriEco
